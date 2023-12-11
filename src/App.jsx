9fafa343a22e0de220fef346fd763c5b3db4ff0c
9barracudA9 <mny2377@gmail.com>
2023-12-11 21:50:31 +0100

    props problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 import './App.css';
-import {quizData} from "./data/questions";
-import {useState} from "react";
-import Game from './components/Game'
-import ResultBox from "./components/ResultBox";
+import FullQuiz from "./components/FullQuiz";
 
 
 const App = () => {
-    const [step, setStep] = useState(0)
-    const question = quizData[step]
-    const percentage = Math.round((step + 1) / quizData.length * 100)
 
-    const [answer, setAnswer] = useState(0)
-
-    const reloadQuiz = () => {
-        setStep((step) => step = 0)
-        setAnswer((answer) => answer = 0)
-    }
-    const nextQuestion = (index) => {
-        setStep(step + 1)
-        if (index === question.correct) {
-            setAnswer(answer + 1)
-        }
-
-
-    }
     return (
-        step !== quizData.length ? (
-            <Game nextQuestion={nextQuestion} percentage={percentage} question={question}/>
-        ) : (<ResultBox answer={answer} reloadQuiz={reloadQuiz}/>
 
-        )
+        <FullQuiz reloadQuiz={reloadQuiz} answer={answer} question={question} percentage={percentage}
+                  nextQuestion={nextQuestion}/>
+        // <div className='bg-blue-300 border-2 border-white w-[450px] rounded-2xl px-4 py-2 '>
+        //     <h1 className='font-bold text-2xl text-cyan-950  italic text-center'>Invite people!</h1>
+        //     <ul className='mt-[40px]'>
+        //         <li className='flex justify-between items-center py-2 px-4 border-2 border-white rounded-2xl hover:bg-amber-50 transition 0.3s '>
+        //             <div className='flex'>
+        //                 <img className="w-[50px] h-[50px] rounded-3xl"
+        //                      src="https://reqres.in/img/faces/1-image.jpg" alt="User"
+        //                 />
+        //                 <div className='flex flex-col  ml-9'>
+        //                     <h3>John Stockton</h3>
+        //                     <p className='opacity-60 text-sm'>example777@gmail.com</p>
+        //                 </div>
+        //             </div>
+        //
+        //             <button className='w-[35px] h-[35px] rounded-3xl bg-green-500'>
+        //
+        //             </button>
+        //         </li>
+        //     </ul>
+        // </div>
     )
 }
 export default App;
---
+++ src/components/FullQuiz.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {quizData} from "../data/questions";
+import Game from "./Game";
+import ResultBox from "./ResultBox";
+
+const FullQuiz = ({nextQuestion, percentage, question, answer, reloadQuiz}) => {
+    const [step, setStep] = useState(0)
+    const question = quizData[step]
+    const percentage = Math.round((step + 1) / quizData.length * 100)
+
+    const [answer, setAnswer] = useState(0)
+
+    const reloadQuiz = () => {
+        setStep((step) => step = 0)
+        setAnswer((answer) => answer = 0)
+    }
+    const nextQuestion = (index) => {
+        setStep(step + 1)
+        if (index === question.correct) {
+            setAnswer(answer + 1)
+        }
+    }
+}
+return (
+    step !== quizData.length ? (
+        <Game nextQuestion={nextQuestion} percentage={percentage} question={question}/>
+    ) : (<ResultBox answer={answer} reloadQuiz={reloadQuiz}/>
+    )
+)
+
+
+export default FullQuiz;
